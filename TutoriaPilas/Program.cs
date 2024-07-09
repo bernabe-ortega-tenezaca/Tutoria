077675808416185db98334c56ed2650d1123046a
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2024-07-09 18:43:12 -0500

    Definición de pila y agrega materias

--- TutoriaPilas/Program.cs
+++ TutoriaPilas/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
 Console.WriteLine("Hello, World!");
+
+// Definir una pila de cadenas
+Stack<string> materias = new Stack<string>();
+// Otra forma de crear una pila
+var otraPila = new Stack<string>();
+
+System.Console.WriteLine("Ingres una materia");
+string materia = Console.ReadLine();
+
+materias.Push(materia);
+materias.Push("Quimica");
+materias.Push("Física");
+
