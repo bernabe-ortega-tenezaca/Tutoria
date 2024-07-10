2a10bf109c2de8f8a0f8a9560445a3fe095bde0c
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2024-07-09 19:10:05 -0500

    Llenar pila desde teclado

--- TutoriaPilas/Program.cs
+++ TutoriaPilas/Program.cs
@@ -8,19 +8,27 @@ Stack<string> materias = new Stack<string>();
 // Otra forma de crear una pila
 var otraPila = new Stack<string>();
 
-System.Console.WriteLine("Ingres una materia");
-//string materia = Console.ReadLine();
-
 //Agregar datos a la pila
 materias.Push("Matemáticas"); // Matemáticas se va al fondo
 materias.Push("Química"); 
 materias.Push("Física"); // Fisica será el top de los elementos
+
+// Recorre la pila y la imprime
+// foreach (var item in materias){
+//     System.Console.WriteLine(item);
+// }
 imprimirPila(materias);
 
+// pila con lectura de datos
+for(int i =1; i<=5; i++){
+    System.Console.Write("Ingrese un nombre: ");
+    otraPila.Push(Console.ReadLine());
+}
 
-
+imprimirPila(otraPila);
 
 static void imprimirPila(Stack<string> pila){
+    System.Console.WriteLine("**************************");
     foreach (var item in pila){
         System.Console.WriteLine(item);
     }
